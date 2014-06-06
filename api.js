75b091ce84f890669a6fa4ab2c8c643c718321ed
s-ko <aleks@s-ko.net>
2014-06-06 20:19:49 +0100

    Added generated documentation missing files

---
+++ api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "LogHarvester",
+        "LogNode",
+        "LogServer",
+        "LogStream",
+        "WebServer"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
